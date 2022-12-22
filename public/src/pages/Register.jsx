325a23e80c6da62636fad9bea39766f847576a51
Jatin1053 <tanishqjagwani0@gmail.com>
2022-12-22 21:16:01 +0530

    First Commit

---
+++ public/src/pages/Register.jsx
@@ -0,0 +1,119 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useCookies } from "react-cookie";
+import { Link, useNavigate } from "react-router-dom";
+function Register() {
+  const [cookies] = useCookies(["cookie-name"]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (cookies.jwt) {
+      navigate("/");
+    }
+  }, [cookies, navigate]);
+
+  const [values, setValues] = useState({
+    Name: "",
+    PhoneNumber: "",
+    email: "",
+    password: "",
+    Linkedinn: "",
+  });
+  const generateError = (error) =>
+    toast.error(error, {
+      position: "bottom-right",
+    });
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/register",
+        {
+          ...values,
+        },
+        { withCredentials: true }
+      );
+      if (data) {
+        if (data.errors) {
+          const { email, password } = data.errors;
+          if (email) generateError(email);
+          else if (password) generateError(password);
+        } else {
+          navigate("/");
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+  return (
+    <div className="container">
+      <h2>Register Account</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label htmlFor="PhoneNumber">Phone-Number</label>
+          <input
+            type="PhoneNumber"
+            name="PhoneNumber"
+            placeholder="PhoneNumber"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="Name">Email</label>
+          <input
+            type="Name"
+            name="Name"
+            placeholder="Name"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="Linkedinn">Linkedinn URL </label>
+          <input
+            type="Linkedinn"
+            name="Linkedinn"
+            placeholder="Linkedinn"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account ?<Link to="/login"> Login</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default Register;
